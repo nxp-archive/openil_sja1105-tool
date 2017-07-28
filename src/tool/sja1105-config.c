nxp-archive/openil_sja1105-tool

commit 26c2a53286dae40201e06ed6ebc1bb59602c77d7

lib: tool: export SJA1105T device id to the config file
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

etc/sja1105.conf
@@ -31,6 +31,7 @@
 
 [spi-setup]
 	staging-area = /lib/firmware/sja1105.bin
+	device_id    = 0x9e00030e
 	device       = /dev/spidev0.1
 	bits         = 8
 	speed        = 1000000

src/tool/sja1105-config.c
@@ -40,6 +40,7 @@
 
 const char *default_staging_area = "/etc/sja1105/.staging";
 const char *default_device = "/dev/spidev0.1";
+const uint64_t default_device_id = SJA1105T_DEVICE_ID;
 
 struct general_config general_config;
 int SJA1105_VERBOSE_CONDITION;
@@ -67,6 +68,12 @@ static inline int parse_spi_setup(struct sja1105_spi_setup *spi_setup, char *key
 
 	if (strcmp(key, "device") == 0) {
 		spi_setup->device = strdup(value);
+	} else if (strcmp(key, "device-id") == 0) {
+		rc = reliable_uint64_from_string(&tmp, value, NULL);
+		if (rc < 0) {
+			goto error;
+		}
+		spi_setup->device_id = tmp;
 	} else if (strcmp(key, "bits") == 0) {
 		rc = reliable_uint64_from_string(&tmp, value, NULL);
 		if (rc < 0) {
@@ -265,6 +272,7 @@ out:
 default_conf:
 	if (rc == -1) {
 		loge("Invalid config file, using defaults.\n");
+		spi_setup->device_id    = default_device_id;
 		spi_setup->device       = default_device;
 		spi_setup->staging_area = default_staging_area;
 		spi_setup->mode         = 0;

src/lib/include/config-tables.h
@@ -103,7 +103,7 @@
 #define MAX_FRAME_MEMORY                         929
 #define MAX_FRAME_MEMORY_RETAGGING               910
 
-#define SJA1105_DEVICE_ID                        0x9E00030Eull
+#define SJA1105T_DEVICE_ID                       0x9E00030Eull
 
 struct sja1105_schedule_entry {
 	uint64_t winstindex;

src/lib/include/spi.h
@@ -35,6 +35,7 @@
 #include <stdint.h>
 
 struct sja1105_spi_setup {
+	uint64_t    device_id;
 	const char *device;
 	uint8_t     mode;
 	uint8_t     bits;

src/tool/parse-config.c
@@ -224,7 +224,6 @@ int config_upload(struct sja1105_spi_setup *spi_setup, struct sja1105_static_con
 {
 	struct   sja1105_table_header final_header;
 	char    *final_header_ptr;
-	uint64_t device_id = SJA1105_DEVICE_ID;
 	/* XXX: Maybe 100 is not the best number of chunks here */
 	struct sja1105_spi_chunk chunks[100];
 	int    chunk_count;
@@ -244,7 +243,8 @@ int config_upload(struct sja1105_spi_setup *spi_setup, struct sja1105_static_con
 		goto out;
 	}
 	/* Write Device ID to first 4 bytes of config_buf */
-	rc = gtable_pack(config_buf, &device_id, 31, 0, SIZE_SJA1105_DEVICE_ID);
+	rc = gtable_pack(config_buf, &spi_setup->device_id, 31, 0,
+	                 SIZE_SJA1105_DEVICE_ID);
 	if (rc < 0) {
 		loge("failed to write device id to buffer");
 		goto out_free;
@@ -326,7 +326,7 @@ int config_flush(struct sja1105_spi_setup *spi_setup, struct sja1105_static_conf
 {
 	struct sja1105_reset_ctrl     reset = {.rst_ctrl = RGU_COLD};
 	struct sja1105_general_status status;
-	uint64_t expected_device_id = SJA1105_DEVICE_ID;
+	uint64_t expected_device_id = spi_setup->device_id;
 	int rc;
 
 	/* Check that we are talking with the right device over SPI */

commit f2697421a9b3fc9bbe7d7e6697c620c965c6192e

conf: tool: init: replace all "-" signs with "_" to avoid confusion
* this also fixes bug introduced by "26c2a53 lib: tool: export SJA1105T
  device id to the config file" where there was a mismatch between the
  variable name in the code (device_id) and the string in the conf file
  that the code was actually looking for (device-id).
* although harmless, sja1105-tool was complaining that "device_id field
  not defined in config file, setting default value 0x9e00030e", and
  upon setting it, that "Invalid key device_id", because it was in fact
  looking for "device-id"

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

etc/sja1105-init
@@ -8,7 +8,7 @@
 
 start() {
 	printf "Starting SJA1105 TSN switch: "
-	staging_area=`cat /etc/sja1105/sja1105.conf | awk '/staging-area/ {print $3;}'`
+	staging_area=`cat /etc/sja1105/sja1105.conf | awk '/staging_area/ {print $3;}'`
 	if ! [[ -e $staging_area ]]; then
 		echo "Warning, no config present at \"$staging_area\", creating default"
 		mkdir -p "$(dirname $staging_area)"

etc/sja1105.conf
@@ -29,8 +29,8 @@
 # POSSIBILITY OF SUCH DAMAGE.
 ##############################################################################
 
-[spi-setup]
-	staging-area = /lib/firmware/sja1105.bin
+[spi_setup]
+	staging_area = /lib/firmware/sja1105.bin
 	device_id    = 0x9e00030e
 	device       = /dev/spidev0.1
 	bits         = 8
@@ -42,7 +42,7 @@
 	auto_flush   = false
 
 [general]
-	screen-width     = 120
-	entries-per-line = 10
+	screen_width     = 120
+	entries_per_line = 10
 	verbose          = false
 	debug            = false

src/tool/sja1105-config.c
@@ -115,7 +115,7 @@ parse_spi_setup(struct sja1105_spi_setup *spi_setup, char *key, char *value,
 	if (strcmp(key, "device") == 0) {
 		spi_setup->device = strdup(value);
 		fields_set->device = 1;
-	} else if (strcmp(key, "device-id") == 0) {
+	} else if (strcmp(key, "device_id") == 0) {
 		rc = reliable_uint64_from_string(&tmp, value, NULL);
 		if (rc < 0) {
 			goto error;
@@ -186,7 +186,7 @@ parse_spi_setup(struct sja1105_spi_setup *spi_setup, char *key, char *value,
 			return -1;
 		}
 		fields_set->flush = 1;
-	} else if (strcmp(key, "staging-area") == 0) {
+	} else if (strcmp(key, "staging_area") == 0) {
 		spi_setup->staging_area = strdup(value);
 		fields_set->staging_area = 1;
 	} else {
@@ -228,14 +228,14 @@ parse_general_config(struct general_config *general_conf,
 			return -1;
 		}
 		fields_set->debug = 1;
-	} else if (strcmp(key, "entries-per-line") == 0) {
+	} else if (strcmp(key, "entries_per_line") == 0) {
 		rc = reliable_uint64_from_string(&tmp, value, NULL);
 		if (rc < 0) {
 			goto error;
 		}
 		general_conf->entries_per_line = tmp;
 		fields_set->entries_per_line = 1;
-	} else if (strcmp(key, "screen-width") == 0) {
+	} else if (strcmp(key, "screen_width") == 0) {
 		rc = reliable_uint64_from_string(&tmp, value, NULL);
 		if (rc < 0) {
 			goto error;
@@ -257,7 +257,7 @@ static inline int parse_key_val(struct sja1105_spi_setup *spi_setup,
                                 char *key, char *value, char *section_hdr,
                                 struct fields_set *fields_set)
 {
-	if (strcmp(section_hdr, "[spi-setup]") == 0) {
+	if (strcmp(section_hdr, "[spi_setup]") == 0) {
 		parse_spi_setup(spi_setup, key, value, fields_set);
 	} else if (strcmp(section_hdr, "[general]") == 0) {
 		parse_general_config(general_conf, key, value, fields_set);