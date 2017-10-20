f4f55ca95a9d911b2adc879e08a9fd1e68712795
Vladimir Oltean <vladimir.oltean@nxp.com>
2017-10-20 16:36:57 +0300

    lib: tool: rework ptp api
    * convert time parameters from uint64_t to struct timespec
    * remove code for interpreting the ptp settings in the staging area
      (qbv schedule autostart, etc). "Autostart" is the incorrect way to
      perform Qbv synchronization with PTP, so these settings are invalid
      anyway. A correct approach is to monitor PTPCLKVAL and set PTPSCHTM
      accordingly, dynamically, which is something that ptp4l now does, by
      calling into libsja1105.
    * since sja1105 management code (such as sja1105.c from ptp4l) might
      eventually be moved into the kernel, we are keeping the ptp
      infrastructure from the staging area (it has to be rewritten, but
      not removing it just yet).
    * for the moment, keeping "sja1105-tool ptp *" commands in a broken state
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

---
+++ src/lib/ptp/ptp.c
@@ -0,0 +1,410 @@
+/******************************************************************************
+ * Copyright (c) 2017, NXP Semiconductors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+#include <string.h>
+#include <inttypes.h>
+#include <errno.h>
+/* These are our own include files */
+#include <lib/include/ptp.h>
+#include <lib/include/gtable.h>
+#include <lib/include/status-tables.h>
+#include <lib/include/spi.h>
+#include <common.h>
+
+#define NSEC_PER_SEC 1000000000LL
+
+static void sja1105_ptp_cmd_access(
+		void *buf,
+		struct sja1105_ptp_cmd *ptp_cmd,
+		int write)
+{
+	int  (*pack_or_unpack)(void*, uint64_t*, int, int, int);
+	int    size = 4;
+	/* No need to keep this as part of the structure */
+	uint64_t valid = 1;
+
+	if (write == 0) {
+		pack_or_unpack = gtable_unpack;
+		memset(ptp_cmd, 0, sizeof(*ptp_cmd));
+	} else {
+		pack_or_unpack = gtable_pack;
+		memset(buf, 0, size);
+	}
+	pack_or_unpack(buf, &valid,               31, 31, 4);
+	pack_or_unpack(buf, &ptp_cmd->ptpstrtsch, 30, 30, 4);
+	pack_or_unpack(buf, &ptp_cmd->ptpstopsch, 29, 29, 4);
+	pack_or_unpack(buf, &ptp_cmd->startptpcp, 28, 28, 4);
+	pack_or_unpack(buf, &ptp_cmd->stopptpcp,  27, 27, 4);
+	pack_or_unpack(buf, &ptp_cmd->resptp,      2,  2, 4);
+	pack_or_unpack(buf, &ptp_cmd->corrclk4ts,  1,  1, 4);
+	pack_or_unpack(buf, &ptp_cmd->ptpclkadd,   0,  0, 4);
+}
+
+void sja1105_ptp_cmd_pack(void *buf,
+                          struct sja1105_ptp_cmd *ptp_cmd)
+{
+	sja1105_ptp_cmd_access(buf, ptp_cmd, 1);
+}
+
+void sja1105_ptp_cmd_unpack(void *buf,
+                            struct sja1105_ptp_cmd *ptp_cmd)
+{
+	sja1105_ptp_cmd_access(buf, ptp_cmd, 0);
+}
+
+void sja1105_ptp_cmd_show(struct sja1105_ptp_cmd *ptp_cmd)
+{
+	printf("PTPSTRTSCH %" PRIX64 "\n", ptp_cmd->ptpstrtsch);
+	printf("PTPSTOPSCH %" PRIX64 "\n", ptp_cmd->ptpstopsch);
+	printf("STARTPTPCP %" PRIX64 "\n", ptp_cmd->startptpcp);
+	printf("STOPPTPCP  %" PRIX64 "\n", ptp_cmd->stopptpcp);
+	printf("RESPTP     %" PRIX64 "\n", ptp_cmd->resptp);
+	printf("CORRCLK4TS %" PRIX64 "\n", ptp_cmd->corrclk4ts);
+	printf("PTPCLKADD  %" PRIX64 "\n", ptp_cmd->ptpclkadd);
+}
+
+/* Wrapper around sja1105_spi_send_packed_buf() */
+int sja1105_ptp_cmd_commit(struct sja1105_spi_setup *spi_setup,
+                           struct sja1105_ptp_cmd *ptp_cmd)
+{
+	const int PTP_CONTROL_ADDR = 0x17;
+	const int BUF_LEN = 4;
+	uint8_t packed_buf[BUF_LEN];
+
+	sja1105_ptp_cmd_pack(packed_buf, ptp_cmd);
+	return sja1105_spi_send_packed_buf(spi_setup,
+	                                   SPI_WRITE,
+	                                   CORE_ADDR + PTP_CONTROL_ADDR,
+	                                   packed_buf,
+	                                   BUF_LEN);
+};
+
+int sja1105_ptp_qbv_start(struct sja1105_spi_setup *spi_setup)
+{
+	struct sja1105_ptp_cmd ptp_cmd;
+
+	memset(&ptp_cmd, 0, sizeof(ptp_cmd));
+	ptp_cmd.ptpstrtsch = 1;
+	return sja1105_ptp_cmd_commit(spi_setup, &ptp_cmd);
+}
+
+int sja1105_ptp_qbv_stop(struct sja1105_spi_setup *spi_setup)
+{
+	struct sja1105_ptp_cmd ptp_cmd;
+
+	memset(&ptp_cmd, 0, sizeof(ptp_cmd));
+	ptp_cmd.ptpstopsch = 1;
+	return sja1105_ptp_cmd_commit(spi_setup, &ptp_cmd);
+}
+
+int sja1105_ptp_pin_toggle_start(struct sja1105_spi_setup *spi_setup)
+{
+	struct sja1105_ptp_cmd ptp_cmd;
+
+	memset(&ptp_cmd, 0, sizeof(ptp_cmd));
+	ptp_cmd.startptpcp = 1;
+	return sja1105_ptp_cmd_commit(spi_setup, &ptp_cmd);
+}
+
+int sja1105_ptp_pin_toggle_stop(struct sja1105_spi_setup *spi_setup)
+{
+	struct sja1105_ptp_cmd ptp_cmd;
+
+	memset(&ptp_cmd, 0, sizeof(ptp_cmd));
+	ptp_cmd.stopptpcp = 1;
+	return sja1105_ptp_cmd_commit(spi_setup, &ptp_cmd);
+}
+
+int sja1105_ptp_reset(struct sja1105_spi_setup *spi_setup)
+{
+	struct sja1105_ptp_cmd ptp_cmd;
+
+	memset(&ptp_cmd, 0, sizeof(ptp_cmd));
+	ptp_cmd.resptp = 1;
+	logv("Resetting PTP clock");
+	return sja1105_ptp_cmd_commit(spi_setup, &ptp_cmd);
+}
+
+static inline int
+sja1105_ptp_add_mode_set(struct sja1105_spi_setup *spi_setup,
+                         enum sja1105_ptp_clk_add_mode mode)
+{
+	struct sja1105_ptp_cmd ptp_cmd;
+
+	memset(&ptp_cmd, 0, sizeof(ptp_cmd));
+	ptp_cmd.ptpclkadd = mode;
+	return sja1105_ptp_cmd_commit(spi_setup, &ptp_cmd);
+}
+
+/* Wrapper around sja1105_spi_send_int() using SPI_READ
+ * at CORE_ADDR + reg_offset */
+static inline int
+sja1105_ptp_read_reg(struct sja1105_spi_setup *spi_setup,
+                     uint64_t reg_offset,
+                     uint64_t *value,
+                     uint64_t size_bytes)
+{
+	return sja1105_spi_send_int(spi_setup,
+	                            SPI_READ,
+	                            CORE_ADDR + PTP_ADDR + reg_offset,
+	                            value,
+	                            size_bytes);
+}
+
+/* Wrapper around sja1105_spi_send_int() using SPI_WRITE
+ * at CORE_ADDR + reg_offset */
+static inline int
+sja1105_ptp_write_reg(struct sja1105_spi_setup *spi_setup,
+                      uint64_t reg_offset,
+                      uint64_t *value,
+                      uint64_t size_bytes)
+{
+	return sja1105_spi_send_int(spi_setup,
+	                            SPI_WRITE,
+	                            CORE_ADDR + PTP_ADDR + reg_offset,
+	                            value,
+	                            size_bytes);
+}
+
+/* Read PTPTSCLK */
+int sja1105_ptp_ts_clk_get(struct sja1105_spi_setup *spi_setup,
+                           struct timespec *ts)
+{
+	uint64_t ptptsclk;
+	int rc;
+
+	rc = sja1105_ptp_read_reg(spi_setup,
+	                          SJA1105_PTPTSCLK_ADDR,
+	                          &ptptsclk, 8);
+	if (rc < 0) {
+		loge("%s: failed to read ptptsclk", __func__);
+		goto out;
+	}
+	sja1105_ptp_time_to_timespec(ts, ptptsclk);
+out:
+	return rc;
+}
+
+/* Read PTPCLKVAL */
+int sja1105_ptp_clk_get(struct sja1105_spi_setup *spi_setup,
+                        struct timespec *ts)
+{
+	uint64_t ptpclkval;
+	int rc;
+
+	rc = sja1105_ptp_read_reg(spi_setup,
+	                          SJA1105_PTPCLKVAL_ADDR,
+	                          &ptpclkval, 8);
+	if (rc < 0) {
+		loge("%s: failed to read ptpclkval", __func__);
+		goto out;
+	}
+	sja1105_ptp_time_to_timespec(ts, ptpclkval);
+out:
+	return rc;
+}
+
+void sja1105_timespec_to_ptp_time(const struct timespec *ts, uint64_t *ptp_time)
+{
+	*ptp_time = (ts->tv_sec * NSEC_PER_SEC + ts->tv_nsec) / 8;
+}
+
+void sja1105_ptp_time_to_timespec(struct timespec *ts, uint64_t ptp_time)
+{
+	ptp_time *= 8;
+	ts->tv_sec  = ptp_time / NSEC_PER_SEC;
+	ts->tv_nsec = ptp_time % NSEC_PER_SEC;
+}
+
+static inline int
+sja1105_ptp_clk_write(struct sja1105_spi_setup *spi_setup,
+                      const struct timespec *ts)
+{
+	uint64_t ptpclkval;
+	sja1105_timespec_to_ptp_time(ts, &ptpclkval);
+	return sja1105_ptp_write_reg(spi_setup,
+	                             SJA1105_PTPCLKVAL_ADDR,
+	                             &ptpclkval, 8);
+}
+
+/* Write to PTPCLKVAL while PTPCLKADD is 0 */
+int sja1105_ptp_clk_set(struct sja1105_spi_setup *spi_setup,
+                        const struct timespec *ts)
+{
+	int rc;
+
+	rc = sja1105_ptp_add_mode_set(spi_setup, PTP_SET_MODE);
+	if (rc < 0) {
+		loge("failed configuring set mode for ptp clk");
+		goto out;
+	}
+	rc = sja1105_ptp_clk_write(spi_setup, ts);
+out:
+	return rc;
+}
+
+/* Write to PTPCLKVAL while PTPCLKADD is 1 */
+int sja1105_ptp_clk_add(struct sja1105_spi_setup *spi_setup,
+                        const struct timespec *ts)
+{
+	int rc;
+
+	rc = sja1105_ptp_add_mode_set(spi_setup, PTP_ADD_MODE);
+	if (rc < 0) {
+		loge("failed configuring add mode for ptp clk");
+		goto out;
+	}
+	rc = sja1105_ptp_clk_write(spi_setup, ts);
+out:
+	return rc;
+}
+
+/* IEEE 754 (double precision): fractional part is 52 bits.
+ * Take most significant 31 bits of that.
+ */
+#define DOUBLE_FRACTION_AS_UINT32(double_ptr) \
+	(uint32_t)(((uint64_t) *double_ptr >> (52 - 31)) & 0x7fffffff)
+
+static inline int
+sja1105_ptpclkrate_from_ratio(double ratio, uint32_t *ptpclkrate)
+{
+	uint64_t *ratio_ptr;
+	int rc = 0;
+
+	if (ratio <= 0 || ratio >= 2) {
+		loge("ratio %lf outside of range", ratio);
+		rc = -ERANGE;
+		goto out;
+	}
+	ratio_ptr = (uint64_t *)(&ratio);
+	if (ratio < 1) {
+		/* Put fractional part of proper ratio (larger than 1)
+		 * into result. Integer part (msb) is zero */
+		ratio += 1;
+		*ptpclkrate = DOUBLE_FRACTION_AS_UINT32(ratio_ptr);
+	} else {
+		/* Put fractional part of ratio into result.
+		 * Make integer part (msb) 1 */
+		*ptpclkrate = DOUBLE_FRACTION_AS_UINT32(ratio_ptr);
+		*ptpclkrate |= 0x80000000;
+	}
+out:
+	return rc;
+}
+
+/* Write to PTPCLKRATE */
+int sja1105_ptp_clk_rate_set(struct sja1105_spi_setup *spi_setup,
+                             double ratio)
+{
+	uint32_t ptpclkrate;
+	uint64_t ptpclkrate_ext;
+	int rc;
+
+	rc = sja1105_ptpclkrate_from_ratio(ratio, &ptpclkrate);
+	if (rc < 0) {
+		loge("%s: failed to convert ratio %lf", __func__, ratio);
+		return rc;
+	}
+	ptpclkrate_ext = ptpclkrate;
+	return sja1105_ptp_write_reg(spi_setup,
+	                             SJA1105_PTPCLKRATE_ADDR,
+	                             &ptpclkrate_ext, 4);
+}
+
+/* Write to PTPPINST */
+int sja1105_ptp_pin_start_time_set(struct sja1105_spi_setup *spi_setup,
+                                   const struct timespec *ts)
+{
+	uint64_t pinst;
+	sja1105_timespec_to_ptp_time(ts, &pinst);
+	return sja1105_ptp_write_reg(spi_setup,
+	                             SJA1105_PTPPINST_ADDR,
+	                             &pinst, 8);
+}
+
+/* Write to PTPPINDUR */
+int sja1105_ptp_pin_duration_set(struct sja1105_spi_setup *spi_setup,
+                                 const struct timespec *ts)
+{
+	uint64_t pindur;
+	int rc;
+
+	sja1105_timespec_to_ptp_time(ts, &pindur);
+	if (pindur >= UINT32_MAX) {
+		loge("%s: provided ts is too large", __func__);
+		rc = -1;
+		goto out;
+	}
+	rc = sja1105_ptp_write_reg(spi_setup,
+	                           SJA1105_PTPPINDUR_ADDR,
+	                           &pindur, 4);
+out:
+	return rc;
+}
+
+/* Write to PTPCLKCORP */
+int sja1105_ptp_qbv_correction_period_set(struct sja1105_spi_setup *spi_setup,
+                                          const struct timespec *ts)
+{
+	uint64_t ptpclkcorp;
+	int rc;
+
+	sja1105_timespec_to_ptp_time(ts, &ptpclkcorp);
+	if (ptpclkcorp >= UINT32_MAX) {
+		loge("%s: provided ts is too large", __func__);
+		rc = -1;
+		goto out;
+	}
+	rc = sja1105_ptp_write_reg(spi_setup,
+	                           SJA1105_PTPCLKCORP_ADDR,
+	                           &ptpclkcorp, 4);
+out:
+	return rc;
+}
+
+/* Write to PTPSCHTM */
+int sja1105_ptp_qbv_start_time_set(struct sja1105_spi_setup *spi_setup,
+                                   const struct timespec *ts)
+{
+	uint64_t ptpschtm;
+	sja1105_timespec_to_ptp_time(ts, &ptpschtm);
+	return sja1105_ptp_write_reg(spi_setup,
+	                             SJA1105_PTPSCHTM_ADDR,
+	                             &ptpschtm, 8);
+}
+
+int sja1105_ptp_configure(__attribute__((unused)) struct sja1105_spi_setup* spi_setup,
+                          __attribute__((unused)) struct sja1105_ptp_config* ptp_config) { return -1; }
+
+void sja1105_ptp_config_unpack(__attribute__((unused)) void *buf, __attribute__((unused)) struct sja1105_ptp_config *ptp_config) {}
+
+void sja1105_ptp_config_pack(__attribute__((unused)) void *buf, __attribute__((unused)) struct sja1105_ptp_config *ptp_config) {}
+void sja1105_ptp_config_fmt_show(__attribute__((unused)) char *print_buf, __attribute__((unused)) char *fmt, __attribute__((unused)) struct sja1105_ptp_config *ptp_config) {}
+void sja1105_ptp_config_show(__attribute__((unused)) struct sja1105_ptp_config *ptp_config) {}
