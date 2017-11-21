1a609b51d77c55776cb81950eaf32a2296a99760
Vladimir Oltean <vladimir.oltean@nxp.com>
2017-11-22 01:22:48 +0200

    lib: implement management route creation and removal
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

---
+++ src/lib/include/dynamic-config.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright (c) 2017, NXP Semiconductors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+#ifndef _DYN_CFG_H
+#define _DYN_CFG_H
+
+#include "dynamic-config-tables.h"
+#include "spi.h"
+
+void sja1105_dyn_l2_lookup_cmd_pack(void *buf, struct
+                                    sja1105_dyn_l2_lookup_cmd *cmd);
+void sja1105_dyn_l2_lookup_cmd_unpack(void *buf, struct
+                                      sja1105_dyn_l2_lookup_cmd *cmd);
+int sja1105_mgmt_route_get(struct sja1105_spi_setup*, struct sja1105_mgmt_entry*, int index);
+int sja1105_mgmt_route_set(struct sja1105_spi_setup*, struct sja1105_mgmt_entry*, int index);
+void sja1105_mgmt_entry_show(struct sja1105_mgmt_entry *entry);
+
+#endif
