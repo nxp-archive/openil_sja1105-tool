f528a8fa751f42ac2a5c76a0f890db828cd7ea51
Vladimir Oltean <vladimir.oltean@nxp.com>
2018-05-15 18:50:08 +0300

    lib: tool: remap error codes to a clear set exported to other programs
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- src/lib/include/errors.h
+++ src/lib/include/errors.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2016, NXP Semiconductors
+ * Copyright (c) 2018, NXP Semiconductors
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,57 +28,22 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *****************************************************************************/
-#ifndef _SJA1105_TOOL_COMMON_H
-#define _SJA1105_TOOL_COMMON_H
+#ifndef _SJA1105_TOOL_ERRORS_H
+#define _SJA1105_TOOL_ERRORS_H
 
-#include <stdint.h>
-#include <stdio.h>
-#include <errno.h>
-
-/* These are our own error codes */
-#include <lib/include/errors.h>
-
-/* Since remapping is used internally, and many checks
- * search for a negative return code, we do that here.
- * The sign is flipped again when returning the error
- * to userspace.
+/* Error codes returned to external userspace applications.
  */
-#define sja1105_err_remap(old_err, new_err)           \
-	do {                                          \
-		logv("Remapping error code %d to %d", \
-		    (old_err), (new_err));            \
-		old_err = -new_err;                   \
-	} while (0);
+#define SJA1105_ERR_OK                                                0
+#define SJA1105_ERR_USAGE                                             1
+#define SJA1105_ERR_CMDLINE_PARSE                                     2
+#define SJA1105_ERR_HW_NOT_RESPONDING                                 3
+#define SJA1105_ERR_HW_NOT_RESPONDING_STAGING_AREA_DIRTY              4
+#define SJA1105_ERR_UPLOAD_FAILED_HW_LEFT_FLOATING                    5
+#define SJA1105_ERR_UPLOAD_FAILED_HW_LEFT_FLOATING_STAGING_AREA_DIRTY 6
+#define SJA1105_ERR_STAGING_AREA_INVALID                              7
+#define SJA1105_ERR_INVALID_XML                                       8
+#define SJA1105_ERR_FILESYSTEM                                        9
 
-#define MAX_LINE_SIZE 2048
-
-/* Macros for conditional, error, verbose and debug logging */
-extern int SJA1105_DEBUG_CONDITION;
-extern int SJA1105_VERBOSE_CONDITION;
-
-#define _log(file, fmt, ...) do { \
-	if (SJA1105_DEBUG_CONDITION) { \
-		fprintf(file, "%s@%d: " fmt "\n", \
-		__func__, __LINE__, ##__VA_ARGS__); \
-	} else { \
-		fprintf(file, fmt "\n", ##__VA_ARGS__); \
-	} \
-} while(0);
-
-#define logc(file, condition, ...) do { \
-	if (condition) { \
-		_log(file, __VA_ARGS__); \
-	} \
-} while(0);
-
-#define loge(...) _log(stderr, __VA_ARGS__)
-#define logi(...) _log(stdout, __VA_ARGS__)
-#define logv(...) logc(stdout, SJA1105_VERBOSE_CONDITION, __VA_ARGS__);
-
-void formatted_append(char *buffer, char *width_fmt, char *fmt, ...);
-void print_array(char *print_buf, uint64_t *array, int count);
-
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
-#define min(x, y) (((x) < (y)) ? (x) : (y))
+const char *sja1105_err_code_to_string(int rc);
 
 #endif
