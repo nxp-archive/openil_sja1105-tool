ad5c097d5483f033c07464bbd8f666ed20a5ddb7
Vladimir Oltean <vladimir.oltean@nxp.com>
2018-08-02 01:45:34 +0300

    lib: General Parameters table entries are different E/T <-> P/Q/R/S
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- src/lib/static-config/static-config.c
+++ src/lib/static-config/static-config.c
@@ -194,8 +194,17 @@ int sja1105_static_config_add_entry(struct sja1105_table_header *hdr, void *buf,
 	}
 	case BLKID_GENERAL_PARAMS_TABLE:
 	{
-		POPULATE_CONFIG_TABLE(general_params, buf, MAX_GENERAL_PARAMS_COUNT, "General Parameters");
-		return SIZE_GENERAL_PARAMS_ENTRY;
+		struct sja1105_general_params_entry entry;
+		CHECK_COUNT(config->general_params_count, MAX_GENERAL_PARAMS_COUNT, "General Parameters");
+		if (IS_ET(config->device_id)) {
+			sja1105et_general_params_entry_unpack(buf, &entry);
+			config->general_params[config->general_params_count++] = entry;
+			return SIZE_GENERAL_PARAMS_ENTRY_ET;
+		} else {
+			sja1105pqrs_general_params_entry_unpack(buf, &entry);
+			config->general_params[config->general_params_count++] = entry;
+			return SIZE_GENERAL_PARAMS_ENTRY_PQRS;
+		}
 	}
 	case BLKID_RETAGGING_TABLE:
 	{
@@ -590,11 +599,19 @@ sja1105_static_config_pack(void *buf, struct sja1105_static_config *config)
 	                     BLKID_AVB_PARAMS_TABLE,
 	                     sja1105_avb_params_entry_pack,
 	                     config->avb_params);
-	PACK_TABLE_IN_BUF_FN(config->general_params_count,
-	                     SIZE_GENERAL_PARAMS_ENTRY,
-	                     BLKID_GENERAL_PARAMS_TABLE,
-	                     sja1105_general_params_entry_pack,
-	                     config->general_params);
+	if (IS_ET(config->device_id)) {
+		PACK_TABLE_IN_BUF_FN(config->general_params_count,
+		                     SIZE_GENERAL_PARAMS_ENTRY_ET,
+		                     BLKID_GENERAL_PARAMS_TABLE,
+		                     sja1105et_general_params_entry_pack,
+		                     config->general_params);
+	} else {
+		PACK_TABLE_IN_BUF_FN(config->general_params_count,
+		                     SIZE_GENERAL_PARAMS_ENTRY_PQRS,
+		                     BLKID_GENERAL_PARAMS_TABLE,
+		                     sja1105pqrs_general_params_entry_pack,
+		                     config->general_params);
+	}
 	PACK_TABLE_IN_BUF_FN(config->xmii_params_count,
 	                     SIZE_XMII_MODE_PARAMS_ENTRY,
 	                     BLKID_XMII_MODE_PARAMS_TABLE,
@@ -652,7 +669,7 @@ sja1105_static_config_get_length(struct sja1105_static_config *config)
 	sum += config->l2_lookup_params_count * (IS_PQRS(config->device_id) ? SIZE_L2_LOOKUP_PARAMS_ENTRY_PQRS : SIZE_L2_LOOKUP_PARAMS_ENTRY_ET);
 	sum += config->l2_forwarding_params_count * SIZE_L2_FORWARDING_PARAMS_ENTRY;
 	sum += config->avb_params_count * SIZE_AVB_PARAMS_ENTRY;
-	sum += config->general_params_count * SIZE_GENERAL_PARAMS_ENTRY;
+	sum += config->general_params_count * (IS_PQRS(config->device_id) ? SIZE_GENERAL_PARAMS_ENTRY_PQRS : SIZE_GENERAL_PARAMS_ENTRY_ET);
 	sum += config->xmii_params_count * SIZE_XMII_MODE_PARAMS_ENTRY;
 	sum -= 4; /* Last header does not have an extra CRC because there is no data */
 	logv("total: %d bytes", sum);
