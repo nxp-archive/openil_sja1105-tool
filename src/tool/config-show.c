74fa3037a359e4a62538d0b6aeb6e5c1db1c093e
Vladimir Oltean <vladimir.oltean@nxp.com>
2018-11-06 15:19:26 +0200

    tool: extend print buffer for vl-forwarding-parameters to 50 characters
    * So that a line covers the PARTSPC field in full.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- src/tool/config-show.c
+++ src/tool/config-show.c
@@ -106,7 +106,7 @@ DECLARE_TABLE_SHOW_FN(vl_lookup, MAX_VL_LOOKUP_COUNT, "Virtual Link Address Look
 DECLARE_TABLE_SHOW_FN(vl_policing, MAX_VL_POLICING_COUNT, "Virtual Link Policing Table:", "%-35s\n")
 DECLARE_TABLE_SHOW_FN(vl_forwarding, MAX_VL_FORWARDING_COUNT, "Virtual Link Forwarding Table", "%-35s\n")
 DECLARE_TABLE_SHOW_FN(avb_params, MAX_AVB_PARAMS_COUNT, "Audio/Video Bridging Parameters Table", "%-35s\n")
-DECLARE_TABLE_SHOW_FN(vl_forwarding_params, MAX_VL_FORWARDING_PARAMS_COUNT, "Virtual Link Forwarding Parameters Table", "%-35s\n")
+DECLARE_TABLE_SHOW_FN(vl_forwarding_params, MAX_VL_FORWARDING_PARAMS_COUNT, "Virtual Link Forwarding Parameters Table", "%-50s\n")
 
 static int
 retagging_table_show(__attribute__((unused)) struct sja1105_static_config *config,
